cd1853f974fe560d3608cfcc43867b3d52938316
Will Salomon <40440339+KingOfPrussia2155@users.noreply.github.com>
2018-09-26 15:02:17 -0600

    semis

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ var path = require("path");
 
 var PORT = process.env.port || 8080;
 
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
-var urlencodedParser = bodyParser.urlencoded({ exnteded: true})
+var urlencodedParser = bodyParser.urlencoded({ exnteded: true});
 
-app.use(bodyParser.json({ type: application*+json }))
+app.use(bodyParser.json({ type: 'application*+json' }));
 
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
 
-app.use(bodyParser.text({ type: 'text/html'}))
+app.use(bodyParser.text({ type: 'text/html'}));
 
-require("./app/routing/api-routes.js")(app);
-require("./app/routing/html-routes.js")(app);
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
   console.log = ("app listen to PORT: " + PORT);
-})
+});
